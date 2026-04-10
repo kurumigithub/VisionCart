75766aae5ba639da282b6f0c08955992c777cec7
quintonking1 <quintonking@berkeley.edu>
2026-04-10 11:34:40 -0700

    maintest setup for ranker & procure

---
+++ src/maintest.py
@@ -0,0 +1,5 @@
+#test to run the procurement and ranker/critic agents with a mock state
+
+#use hard coded personas and style profiles to test the procurement and ranker/critic agents
+
+#can fine tune to make sure the model grabs the correct image from a test dataset
